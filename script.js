b3dd680166bcbe45356f7e1b9a2db169c801ad30
kylengreene <kylengreene@gmail.com>
2020-01-31 10:46:56 -0600

    created loop append

--- script.js
+++ script.js
@@ -10,12 +10,22 @@ console.log('in onReady');
 
 function displaySongs(){
 console.log('in dsiplaySongs');
+    //target an output element by ID
+    let el = $('#songsOut');
+    //empty out element
+    el.empty();
+    //loop through songs array
+    for (song of songs) {
+        //append each song to the dom
+        el.append(`<li> ${song.title} : ${song.artist}, ${song.album} (${song.year}) </li>`);
+    }
+    //append each song to DOM 
 }// end displaySongs
 
 /// - test song
 songs.push({
     artist: 'Red Hot Chili Peppers',
-    song: 'Under the Bridge',
+    title: 'Under the Bridge',
     album: 'Blood Sugar Sex Magik',
     year: 1991
 
