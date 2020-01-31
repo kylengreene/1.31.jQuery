02c569f2eba615898dfe0592a204ed106f1ddc2e
kylengreene <kylengreene@gmail.com>
2020-01-31 10:34:38 -0600

    added button click

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-$('Document').ready(readyNow);
+$('Document').ready(onReady);
 
-function readyNow (){
+//songs app
+let songs =[];
 
-}
+function onReady (){
+console.log('in onReady');
+    $('#updateSongsButton').on('click', displaySongs);
+}//end onReady
+
+function displaySongs(){
+console.log('in dsiplaySongs');
+}// end displaySongs
+
+/// - test song
+songs.push({
+    artist: 'Red Hot Chili Peppers',
+    song: 'Under the Bridge',
+    album: 'Blood Sugar Sex Magik',
+    year: 1991
+
+});
+
