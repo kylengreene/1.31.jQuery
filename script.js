8ad42bd2396f10afae091fda88030f145b1e50d7
kylengreene <kylengreene@gmail.com>
2020-01-31 11:17:40 -0600

    created add song function

--- script.js
+++ script.js
@@ -5,11 +5,30 @@ let songs =[];
 
 function onReady (){
 console.log('in onReady');
-    $('#updateSongsButton').on('click', displaySongs);
+     $('#addSongButton').on('click', addSong);
 }//end onReady
 
+function addSong(){
+    console.log('in addSong');
+    // select inputs by their id use their data in a song object using .val() as a getter
+    let newSong = {
+        artist: $('#artistIn').val(),
+        title: $('#titleIn').val(),
+        album: $('#albumIn').val(),
+        year: $('#yearIn').val()
+    }//end newSong
+    //empty inputs using .val( '' ) as a setter
+    $('#artistIn').val( '' );
+    $('#titleIn').val( '' );
+    $('#albumIn').val( '' );
+    $('#yearIn').val( '' );
+    //push new song object into songs array
+    songs.push (newSong);
+    // display the songs
+    displaySongs();
+}
 function displaySongs(){
-console.log('in dsiplaySongs');
+console.log('in displaySongs');
     //target an output element by ID
     let el = $('#songsOut');
     //empty out element
@@ -17,7 +36,7 @@ console.log('in dsiplaySongs');
     //loop through songs array
     for (song of songs) {
         //append each song to the dom
-        el.append(`<li> ${song.title} : ${song.artist}, ${song.album} (${song.year}) </li>`);
+        el.append(`<li> ${song.title} : ${song.artist}, <nbsp> ${song.album} (${song.year}) </li>`);
     }
     //append each song to DOM 
 }// end displaySongs
